7f25ab2aba1c4ae37b67a6f01b0deb1df796cbb1
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-04-05 23:12:14 +0700

    Update client.c

--- file_transfer_by_c/client.c
+++ file_transfer_by_c/client.c
@@ -1,116 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <fcntl.h>
 
-#define PORT 12345
 #define MAX_BUF_SIZE 1024
+#define PORT 5001
 
 void error(const char *msg)
 {
     perror(msg);
-    exit(1);
+    exit(0);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     int sockfd, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char buffer[MAX_BUF_SIZE];
-    char filename[MAX_BUF_SIZE];
-    off_t filesize;
-    int fd;
 
+    char buffer[MAX_BUF_SIZE];
+
+    // Create a socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    // Get the server IP address
+    printf("Enter server IP address: ");
+    bzero(buffer, MAX_BUF_SIZE);
+    fgets(buffer, MAX_BUF_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
+    server = gethostbyname(buffer);
+    if (server == NULL)
+        error("ERROR, no such host");
+
+    // Set up the server address struct
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(PORT);
+
+    // Connect to the server
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    // Get the filename to download
+    printf("Enter filename to download: ");
+    bzero(buffer, MAX_BUF_SIZE);
+    fgets(buffer, MAX_BUF_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
+
+    // Send the command and filename to the server
+    n = write(sockfd, "DOWNLOAD", 9);
+    if (n < 0)
+        error("ERROR writing to socket");
+
+    n = write(sockfd, buffer, strlen(buffer));
+    if (n < 0)
+        error("ERROR writing to socket");
+
+    // Read the response from the server
+    bzero(buffer, MAX_BUF_SIZE);
+    n = read(sockfd, buffer, MAX_BUF_SIZE);
+    if (n < 0)
+        error("ERROR reading from socket");
+
+    // If the file does not exist on the server, print an error and exit
+    if (strcmp(buffer, "ERROR: FILE NOT FOUND") == 0)
+    {
+        printf("Error: file not found on server.\n");
+        close(sockfd);
+        exit(1);
+    }
+
+    // If the file exists, print the file size and begin downloading the file
+    printf("File size: %s bytes\n", buffer);
+
+    FILE *file = fopen(buffer, "wb");
+    if (file == NULL)
+        error("ERROR creating file");
+
+    // Read the file data from the server and write it to the local file
     while (1)
     {
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0)
-            error("ERROR opening socket");
-
-        printf("Enter server IP address: ");
-        bzero(buffer, MAX_BUF_SIZE);
-        fgets(buffer, MAX_BUF_SIZE - 1, stdin);
-
-        server = gethostbyname(buffer);
-        if (server == NULL)
-        {
-            printf("ERROR, no such host\n");
-            continue;
-        }
-
-        bzero((char *)&serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        bcopy((char *)server->h_addr,
-              (char *)&serv_addr.sin_addr.s_addr,
-              server->h_length);
-        serv_addr.sin_port = htons(PORT);
-
-        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-            error("ERROR connecting");
-
-        printf("Enter command (download <filename>, exit to quit): ");
-        bzero(buffer, MAX_BUF_SIZE);
-        fgets(buffer, MAX_BUF_SIZE - 1, stdin);
-
-        if (strcmp(buffer, "exit\n") == 0)
-        {
-            close(sockfd);
-            break;
-        }
-
-        if (strncmp(buffer, "download", 8) != 0)
-        {
-            printf("Invalid command\n");
-            close(sockfd);
-            continue;
-        }
-
-        bzero(filename, MAX_BUF_SIZE);
-        sscanf(buffer, "download %s", filename);
-
-        n = write(sockfd, buffer, strlen(buffer));
-        if (n < 0)
-            error("ERROR writing to socket");
-
         bzero(buffer, MAX_BUF_SIZE);
         n = read(sockfd, buffer, MAX_BUF_SIZE);
-        if (n < 0)
-            error("ERROR reading from socket");
+        if (n <= 0)
+            break;
 
-        if (strncmp(buffer, "File not found", 14) == 0)
-        {
-            printf("File not found on server\n");
-            close(sockfd);
-            continue;
-        }
-
-        filesize = atol(buffer);
-
-        fd = open(filename, O_CREAT | O_WRONLY, 0666);
-        if (fd < 0)
-            error("ERROR opening file");
-
-        while (filesize > 0)
-        {
-            n = read(sockfd, buffer, MAX_BUF_SIZE);
-            if (n < 0)
-                error("ERROR reading from socket");
-
-            if (write(fd, buffer, n) < 0)
-                error("ERROR writing to file");
-
-            filesize -= n;
-        }
-
-        close(fd);
-        close(sockfd);
-
-        printf("File %s downloaded successfully. Size: %ld bytes\n", filename, filesize);
+        fwrite(buffer, sizeof(char), n, file);
     }
+
+    // Close the file and socket
+    fclose(file);
+    close(sockfd);
+
+    printf("File %s downloaded successfully.\n", buffer);
 
     return 0;
 }
