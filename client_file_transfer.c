793ad2304ba0ec8a5f2d29f02adefa13a83c7e83
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-04-05 22:05:26 +0700

    Create client_file_transfer.c

---
+++ client_file_transfer.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define MAX_BUFFER_SIZE 1024
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[MAX_BUFFER_SIZE] = {0};
+    char *file_name = "received.txt";
+
+    // Create a socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set the server IP address and port number
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive the file contents sent by the server
+    FILE *fp = fopen(file_name, "wb");
+    if (fp == NULL)
+    {
+        perror("File open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int bytes_received;
+    while ((bytes_received = recv(sock, buffer, MAX_BUFFER_SIZE, 0)) > 0)
+    {
+        fwrite(buffer, sizeof(char), bytes_received, fp);
+        memset(buffer, 0, MAX_BUFFER_SIZE);
+    }
+
+    // Close the file and the connection
+    fclose(fp);
+    close(sock);
+
+    return 0;
+}
