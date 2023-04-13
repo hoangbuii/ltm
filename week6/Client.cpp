5ac8742be77a31d74134ba9b0bfc0d8492789487
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-04-13 16:18:29 +0700

    Update Client.cpp

--- week6/Client.cpp
+++ week6/Client.cpp
@@ -22,68 +22,68 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Get server IP address from user input
-    string serverIP;
-    cout << "Enter server IP address: ";
-    cin >> serverIP;
+    // // Get server IP address from user input
+    // string serverIP;
+    // cout << "Enter server IP address: ";
+    // cin >> serverIP;
 
-    // Set server address parameters
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
-    if (inet_pton(AF_INET, serverIP.c_str(), &serverAddress.sin_addr) <= 0) {
-        perror("inet_pton failed");
-        exit(EXIT_FAILURE);
-    }
+    // // Set server address parameters
+    // serverAddress.sin_family = AF_INET;
+    // serverAddress.sin_port = htons(PORT);
+    // if (inet_pton(AF_INET, serverIP.c_str(), &serverAddress.sin_addr) <= 0) {
+    //     perror("inet_pton failed");
+    //     exit(EXIT_FAILURE);
+    // }
 
-    // Connect to server
-    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
-        perror("connect failed");
-        exit(EXIT_FAILURE);
-    }
+    // // Connect to server
+    // if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+    //     perror("connect failed");
+    //     exit(EXIT_FAILURE);
+    // }
 
-    // Send request to server
-    send(clientSocket, "REQUEST", strlen("REQUEST"), 0);
+    // // Send request to server
+    // send(clientSocket, "REQUEST", strlen("REQUEST"), 0);
 
-    // Receive acknowledgement from server
-    memset(buffer, 0, sizeof(buffer));
-    if (recv(clientSocket, buffer, BUFFER_SIZE, 0) < 0) {
-        perror("recv failed");
-        exit(EXIT_FAILURE);
-    }
+    // // Receive acknowledgement from server
+    // memset(buffer, 0, sizeof(buffer));
+    // if (recv(clientSocket, buffer, BUFFER_SIZE, 0) < 0) {
+    //     perror("recv failed");
+    //     exit(EXIT_FAILURE);
+    // }
 
-    // Send filename to server
-    string filename;
-    cout << "Enter filename to download: ";
-    cin >> filename;
-    send(clientSocket, filename.c_str(), filename.length(), 0);
+    // // Send filename to server
+    // string filename;
+    // cout << "Enter filename to download: ";
+    // cin >> filename;
+    // send(clientSocket, filename.c_str(), filename.length(), 0);
 
-    // Receive file size from server
-    int fileSize;
-    memset(buffer, 0, sizeof(buffer));
-    if (recv(clientSocket, (char *)&fileSize, sizeof(int), 0) < 0) {
-        perror("recv failed");
-        exit(EXIT_FAILURE);
-    }
+    // // Receive file size from server
+    // int fileSize;
+    // memset(buffer, 0, sizeof(buffer));
+    // if (recv(clientSocket, (char *)&fileSize, sizeof(int), 0) < 0) {
+    //     perror("recv failed");
+    //     exit(EXIT_FAILURE);
+    // }
 
-    // Display file size and download file
-    cout << "File size: " << fileSize << " bytes" << endl;
-    ofstream file(filename, ios::binary);
-    if (!file.is_open()) {
-        cout << "Could not create file " << filename << endl;
-        exit(EXIT_FAILURE);
-    }
-    int bytesReceived = 0;
-    while (bytesReceived < fileSize) {
-        int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
-        if (bytesRead < 0) {
-            perror("recv failed");
-            exit(EXIT_FAILURE);
-        }
-        file.write(buffer, bytesRead);
-        bytesReceived += bytesRead;
-    }
-    file.close();
-    cout << "File transfer complete" << endl;
+    // // Display file size and download file
+    // cout << "File size: " << fileSize << " bytes" << endl;
+    // ofstream file(filename, ios::binary);
+    // if (!file.is_open()) {
+    //     cout << "Could not create file " << filename << endl;
+    //     exit(EXIT_FAILURE);
+    // }
+    // int bytesReceived = 0;
+    // while (bytesReceived < fileSize) {
+    //     int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+    //     if (bytesRead < 0) {
+    //         perror("recv failed");
+    //         exit(EXIT_FAILURE);
+    //     }
+    //     file.write(buffer, bytesRead);
+    //     bytesReceived += bytesRead;
+    // }
+    // file.close();
+    // cout << "File transfer complete" << endl;
 
     // Close socket and exit client
     close(clientSocket);
