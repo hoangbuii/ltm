85119f7890dbdac9ed0ec017d1906444770a3b03
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-04-13 16:00:54 +0700

    new week6

--- week6/Client.cpp
+++ week6/Client.cpp
@@ -1,139 +1,91 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include <iostream>
+#include <fstream>
+#include <cstring>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <fcntl.h>
-#include <openssl/md5.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-#define BUF_SIZE 1024
+using namespace std;
+
 #define PORT 8080
-
-void md5sum(char *filename, char *md5) {
-    unsigned char c[MD5_DIGEST_LENGTH];
-    int i;
-    FILE *fp;
-    MD5_CTX mdContext;
-    fp = fopen(filename, "rb");
-
-    if (fp == NULL) {
-        perror("fopen failed");
-        exit(EXIT_FAILURE);
-    }
-
-    MD5_Init(&mdContext);
-
-    int bytes;
-    unsigned char data[BUF_SIZE];
-    while ((bytes = fread(data, 1, BUF_SIZE, fp)) != 0) {
-        MD5_Update(&mdContext, data, bytes);
-    }
-
-    MD5_Final(c, &mdContext);
-
-    for(i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        sprintf(&md5[i*2], "%02x", (unsigned int)c[i]);
-    }
-
-    fclose(fp);
-}
+#define BUFFER_SIZE 1024
 
 int main() {
-    struct sockaddr_in server_addr;
-    char buffer[BUF_SIZE];
-    int client_socket, n;
-    ssize_t file_size;
-    socklen_t server_len;
-    char filename[BUF_SIZE];
-    int fd;
-    struct timeval tv;
-    fd_set readfds;
-    int retval;
-    char md5[MD5_DIGEST_LENGTH * 2 + 1];
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
+    int clientSocket, len;
+    struct sockaddr_in serverAddress;
+    char buffer[BUFFER_SIZE] = {0};
 
-    printf("Enter server IP address: ");
-    if (fgets(buffer, BUF_SIZE, stdin) != NULL) {
-        buffer[strcspn(buffer, "\n")] = '\0';
-        if (inet_pton(AF_INET, buffer, &server_addr.sin_addr) <= 0) {
-            perror("inet_pton failed");
+    // Create UDP socket
+    if ((clientSocket = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
+       
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get server IP address from user input
+    string serverIP;
+    cout << "Enter server IP address: ";
+    cin >> serverIP;
+
+    // Set server address parameters
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, serverIP.c_str(), &serverAddress.sin_addr) <= 0) {
+        perror("inet_pton failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("connect failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send request to server
+    send(clientSocket, "REQUEST", strlen("REQUEST"), 0);
+
+    // Receive acknowledgement from server
+    memset(buffer, 0, sizeof(buffer));
+    if (recv(clientSocket, buffer, BUFFER_SIZE, 0) < 0) {
+        perror("recv failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send filename to server
+    string filename;
+    cout << "Enter filename to download: ";
+    cin >> filename;
+    send(clientSocket, filename.c_str(), filename.length(), 0);
+
+    // Receive file size from server
+    int fileSize;
+    memset(buffer, 0, sizeof(buffer));
+    if (recv(clientSocket, (char *)&fileSize, sizeof(int), 0) < 0) {
+        perror("recv failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Display file size and download file
+    cout << "File size: " << fileSize << " bytes" << endl;
+    ofstream file(filename, ios::binary);
+    if (!file.is_open()) {
+        cout << "Could not create file " << filename << endl;
+        exit(EXIT_FAILURE);
+    }
+    int bytesReceived = 0;
+    while (bytesReceived < fileSize) {
+        int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+        if (bytesRead < 0) {
+            perror("recv failed");
             exit(EXIT_FAILURE);
         }
+        file.write(buffer, bytesRead);
+        bytesReceived += bytesRead;
     }
+    file.close();
+    cout << "File transfer complete" << endl;
 
-    if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Enter file name: ");
-    if (fgets(filename, BUF_SIZE, stdin) != NULL) {
-        filename[strcspn(filename, "\n")] = '\0';
-    }
-
-    sendto(client_socket, "DOWNLOAD", strlen("DOWNLOAD"), 0, (const struct sockaddr *)&server_addr, sizeof(server_addr));
-    sendto(client_socket, filename, strlen(filename), 0, (const struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    server_len = sizeof(server_addr);
-    n = recvfrom(client_socket, (char *)&file_size, sizeof(ssize_t), MSG_WAITALL, (struct sockaddr *)&server_addr, &server_len);
-
-    if (n < 0) {
-        perror("recvfrom failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (strcmp((char *)&file_size, "ERROR") == 0) {
-        printf("File not found\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("File size: %ld bytes\n", file_size);
-
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-
-    if (fd < 0) {
-        perror("open failed");
-        exit(EXIT_FAILURE);
-    }
-
-    size_t received_bytes = 0;
-    while (received_bytes < file_size) {
-        tv.tv_sec = 1;
-        tv.tv_usec = 0;
-        FD_ZERO(&readfds);
-        FD_SET(client_socket, &readfds);
-
-        retval = select(client_socket + 1, &readfds, NULL, NULL, &tv);
-
-        if (retval == -1) {
-            perror("select failed");
-            exit(EXIT_FAILURE);
-        } else if (retval == 0) {
-            printf("Timeout waiting for data\n");
-            break;
-        } else {
-            n = recvfrom(client_socket, buffer, BUF_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, &server_len);
-
-            if (n < 0) {
-                perror("recvfrom failed");
-                exit(EXIT_FAILURE);
-            }
-
-            write(fd, buffer, n);
-            received_bytes += n;
-        }
-    }
-
-    close(fd);
-
-    md5sum(filename, md5);
-    printf("MD5 checksum: %s\n", md5);
-
-    close(client_socket);
-
+    // Close socket and exit client
+    close(clientSocket);
     return 0;
 }
--- week6/Server.cpp
+++ week6/Server.cpp
@@ -1,101 +1,83 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define BUF_SIZE 4096
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <port>" << endl;
-        exit(EXIT_FAILURE);
-    }
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-    int port = atoi(argv[1]);
-
-    int server_socket, n;
-    socklen_t client_len;
-    char buffer[BUF_SIZE];
-    struct sockaddr_in server_addr, client_addr;
+int main() {
+    int serverSocket, clientSocket, len;
+    struct sockaddr_in serverAddress, clientAddress;
+    char buffer[BUFFER_SIZE] = {0};
 
     // Create UDP socket
-    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_socket < 0) {
-        perror("Error opening socket");
+    if ((serverSocket = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
+        perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Initialize server address
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
+    // Set server address parameters
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
 
-    // Bind socket to port
-    if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        perror("Error binding socket");
+    // Bind socket to the specified address and port
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    cout << "Server running on port " << port << endl;
+    // Wait for client to connect
+    cout << "Waiting for client to connect..." << endl;
 
-    while (true) {
-        // Receive command and filename from client
-        client_len = sizeof(client_addr);
-        n = recvfrom(server_socket, buffer, BUF_SIZE, 0, (struct sockaddr *) &client_addr, &client_len);
-        if (n < 0) {
-            perror("Error receiving from client");
-            continue;
-        }
+    len = sizeof(clientAddress);
+    memset(buffer, 0, sizeof(buffer));
 
-        string command(buffer, 8);
-        string filename(buffer+8, n-8);
-
-        cout << "Received command: " << command << " filename: " << filename << endl;
-
-        // Open file
-        ifstream file(filename, ios::binary);
-        if (!file) {
-            // Send error message to client if file not found
-            cout << "File not found: " << filename << endl;
-            sendto(server_socket, "ERROR", 5, 0, (struct sockaddr *) &client_addr, client_len);
-            continue;
-        }
-
-        // Get file size
-        file.seekg(0, ios::end);
-        int file_size = file.tellg();
-        file.seekg(0, ios::beg);
-
-        // Send file size to client
-        cout << "File found: " << filename << ", size: " << file_size << endl;
-        sendto(server_socket, to_string(file_size).c_str(), to_string(file_size).size(), 0, (struct sockaddr *) &client_addr, client_len);
-
-        // Send file to client
-        int bytes_sent = 0;
-        while (bytes_sent < file_size) {
-            file.read(buffer, BUF_SIZE);
-            int n_sent = sendto(server_socket, buffer, file.gcount(), 0, (struct sockaddr *) &client_addr, client_len);
-            if (n_sent < 0) {
-                perror("Error sending to client");
-                break;
-            }
-            bytes_sent += n_sent;
-        }
-
-        file.close();
+    // Receive request from client
+    if (recvfrom(serverSocket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientAddress, (socklen_t *)&len) < 0) {
+        perror("recvfrom failed");
+        exit(EXIT_FAILURE);
     }
 
-    close(server_socket);
+    // Send acknowledgement to client
+    sendto(serverSocket, "ACK", strlen("ACK"), 0, (struct sockaddr *)&clientAddress, len);
 
+    // Receive filename from client
+    memset(buffer, 0, sizeof(buffer));
+    if (recvfrom(serverSocket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientAddress, (socklen_t *)&len) < 0) {
+        perror("recvfrom failed");
+        exit(EXIT_FAILURE);
+    }
+    string filename(buffer);
+
+    // Open file and get size
+    ifstream file(filename, ios::binary);
+    if (!file.is_open()) {
+        cout << "File " << filename << " not found" << endl;
+        sendto(serverSocket, "ERROR", strlen("ERROR"), 0, (struct sockaddr *)&clientAddress, len);
+        exit(EXIT_FAILURE);
+    }
+    file.seekg(0, ios::end);
+    int fileSize = file.tellg();
+    file.seekg(0, ios::beg);
+
+    // Send file size to client
+    sendto(serverSocket, (char *)&fileSize, sizeof(int), 0, (struct sockaddr *)&clientAddress, len);
+
+    // Send file to client
+    char fileBuffer[BUFFER_SIZE];
+    int bytesRead;
+    while ((bytesRead = file.readsome(fileBuffer, BUFFER_SIZE)) > 0) {
+        sendto(serverSocket, fileBuffer, bytesRead, 0, (struct sockaddr *)&clientAddress, len);
+    }
+    file.close();
+
+    // Exit server
+    close(serverSocket);
+    cout << "File transfer complete" << endl;
     return 0;
 }
