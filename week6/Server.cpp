b7bc3af3e17cf9be370f0fa4dbc76a484e7a2478
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-04-13 15:45:08 +0700

    Update Server.cpp

--- week6/Server.cpp
+++ week6/Server.cpp
@@ -1,91 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <cstring>
 #include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <fcntl.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define BUF_SIZE 1024
-#define PORT 8080
+#define BUF_SIZE 4096
 
-int main() {
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[BUF_SIZE];
-    int server_socket, client_socket, addr_len, n;
-    ssize_t file_size;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <port>" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+
+    int server_socket, n;
     socklen_t client_len;
-    char filename[BUF_SIZE];
-    int fd;
-    struct stat file_info;
+    char buffer[BUF_SIZE];
+    struct sockaddr_in server_addr, client_addr;
 
+    // Create UDP socket
+    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_socket < 0) {
+        perror("Error opening socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(port);
 
-    if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
-        perror("socket creation failed");
+    // Bind socket to port
+    if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        perror("Error binding socket");
         exit(EXIT_FAILURE);
     }
 
-    if (bind(server_socket, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
+    cout << "Server running on port " << port << endl;
 
-    printf("Server listening on port %d...\n", PORT);
-
-    while (1) {
-        memset(buffer, 0, BUF_SIZE);
-        addr_len = sizeof(client_addr);
-
-        n = recvfrom(server_socket, (char *)buffer, BUF_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &addr_len);
-
+    while (true) {
+        // Receive command and filename from client
+        client_len = sizeof(client_addr);
+        n = recvfrom(server_socket, buffer, BUF_SIZE, 0, (struct sockaddr *) &client_addr, &client_len);
         if (n < 0) {
-            perror("recvfrom failed");
-            exit(EXIT_FAILURE);
+            perror("Error receiving from client");
+            continue;
         }
 
-        buffer[n] = '\0';
+        string command(buffer, 8);
+        string filename(buffer+8, n-8);
 
-        if (strcmp(buffer, "DOWNLOAD") == 0) {
-            printf("Client requested a file...\n");
+        cout << "Received command: " << command << " filename: " << filename << endl;
 
-            n = recvfrom(server_socket, filename, BUF_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &addr_len);
-
-            if (n < 0) {
-                perror("recvfrom failed");
-                exit(EXIT_FAILURE);
-            }
-
-            filename[n] = '\0';
-
-            printf("Client requested file: %s\n", filename);
-
-            if (stat(filename, &file_info) == -1) {
-                printf("File not found\n");
-                sendto(server_socket, "ERROR", strlen("ERROR"), 0, (const struct sockaddr *)&client_addr, addr_len);
-            } else {
-                printf("File found, size: %ld bytes\n", file_info.st_size);
-                sendto(server_socket, (char *)&file_info.st_size, sizeof(ssize_t), 0, (const struct sockaddr *)&client_addr, addr_len);
-                fd = open(filename, O_RDONLY);
-
-                if (fd < 0) {
-                    perror("open failed");
-                    exit(EXIT_FAILURE);
-                }
-
-                while ((n = read(fd, buffer, BUF_SIZE)) > 0) {
-                    sendto(server_socket, buffer, n, 0, (const struct sockaddr *)&client_addr, addr_len);
-                }
-
-                close(fd);
-
-                printf("File sent successfully\n");
-            }
+        // Open file
+        ifstream file(filename, ios::binary);
+        if (!file) {
+            // Send error message to client if file not found
+            cout << "File not found: " << filename << endl;
+            sendto(server_socket, "ERROR", 5, 0, (struct sockaddr *) &client_addr, client_len);
+            continue;
         }
+
+        // Get file size
+        file.seekg(0, ios::end);
+        int file_size = file.tellg();
+        file.seekg(0, ios::beg);
+
+        // Send file size to client
+        cout << "File found: " << filename << ", size: " << file_size << endl;
+        sendto(server_socket, to_string(file_size).c_str(), to_string(file_size).size(), 0, (struct sockaddr *) &client_addr, client_len);
+
+        // Send file to client
+        int bytes_sent = 0;
+        while (bytes_sent < file_size) {
+            file.read(buffer, BUF_SIZE);
+            int n_sent = sendto(server_socket, buffer, file.gcount(), 0, (struct sockaddr *) &client_addr, client_len);
+            if (n_sent < 0) {
+                perror("Error sending to client");
+                break;
+            }
+            bytes_sent += n_sent;
+        }
+
+        file.close();
     }
 
     close(server_socket);
