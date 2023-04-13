7aa01675fbd99996cb4534dcfed5005e418155a0
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-04-13 19:40:39 +0700

    Update Server.cpp

--- Test3/Server.cpp
+++ Test3/Server.cpp
@@ -37,7 +37,7 @@ int main() {
 
     len = sizeof(clientAddress);
     memset(buffer, 0, sizeof(buffer));
-    cout << "Client connected!"
+    
 
     // Receive request from client
         if (recvfrom(serverSocket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientAddress, (socklen_t *)&len) < 0) {
@@ -50,6 +50,8 @@ int main() {
 
         // Send acknowledgement to client
         sendto(serverSocket, "ACK", strlen("ACK"), 0, (struct sockaddr *)&clientAddress, len);
+        
+        cout << "Client connected!";
 
     while (true) {
 
